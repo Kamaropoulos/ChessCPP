d0b5995fd0bb3d660257b0d9365ad7f5d0162120
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2019-07-06 12:19:52 +0300

    feat: Call base constractor constructor from Pawn

--- ChessCPP/Pieces/Pawn/Pawn.cpp
+++ ChessCPP/Pieces/Pawn/Pawn.cpp
@@ -1,1 +1,4 @@
 #include "Pawn.h"
+
+Pawn::Pawn(Color color) : Piece(color) {
+}
--- ChessCPP/Pieces/Pawn/Pawn.h
+++ ChessCPP/Pieces/Pawn/Pawn.h
@@ -2,5 +2,8 @@
 #include "../../Piece/Piece.h"
 class Pawn :
 	public Piece {
+public:
+	Pawn(Color color);
+	string pieceName() { return "Pawn"; }
 };
 
