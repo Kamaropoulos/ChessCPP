f3d61056259ad8259e0fac043fbf2aa0431bcc6c
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2019-07-06 12:35:42 +0300

    feat: Square constructor and hasPiece member function

--- ChessCPP/Square/Square.cpp
+++ ChessCPP/Square/Square.cpp
@@ -1,7 +1,16 @@
 #include "Square.h"
 
+Square::Square(Color color) {
+	this->color = color;
+}
+
 void Square::placePiece(Piece* piece) {
 	this->piece = piece;
+}
+
+bool Square::hasPiece() {
+	if (this->piece != nullptr) return true;
+	return false;
 }
 
 Piece* Square::getPiece() {
