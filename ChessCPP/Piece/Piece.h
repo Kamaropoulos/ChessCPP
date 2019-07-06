f2a06749983cad183f61465783e406894e0d6839
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2019-07-06 12:39:50 +0300

    feat: Add pieceName pure virtual function on Piece base class

--- ChessCPP/Piece/Piece.h
+++ ChessCPP/Piece/Piece.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <string>
+
+using namespace std;
+
 enum Color { WHITE, BLACK };
 
 class Piece {
 public:
 	Color color;
 	Piece(Color color);
+	virtual string pieceName() = 0;
 };
-
