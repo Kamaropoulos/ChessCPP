38fa228e49e359575b6b2259d8408d363bd1b43f
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2019-07-09 13:12:18 +0300

    feat: Added Position class to easily manage positions

---
+++ ChessCPP/Position/Position.cpp
@@ -0,0 +1,109 @@
+#include "Position.h"
+#include <iostream>
+
+int Position::_letterToNumber(char letter) {
+	// Add 1 to start from 1 instead of 0
+	return (int)letter - 'a' + 1;
+}
+
+char Position::_numberToLetter(int number) {
+	return (char)number + 'a' - 1;
+}
+
+pair<int, int> Position::stringToFileAndRank(string notation) {
+	// Convert the first character to a number
+	int file = Position::_letterToNumber(notation.at(0));
+	// Get int from character
+	int rank = notation.at(1) - '0';
+
+	// Return pair of integers
+	// first is file, second is rank
+	return pair<int, int>(file, rank);
+}
+
+string Position::toString() {
+	return this->str;
+}
+
+Position::Position(string posStr) {
+	if (posStr.length() == 2) {
+		int file = this->stringToFileAndRank(posStr).first;
+		int rank = this->stringToFileAndRank(posStr).second;
+
+		if (((file > 0) && (file <= 8)) && ((rank > 0) && (rank <= 8))) {
+			this->str = posStr;
+			this->file = file;
+			this->rank = rank;
+
+			return;
+		}
+	}
+	throw exception("Invalid position string!");
+}
+
+Position::Position(int file, int rank) {
+	if (((file > 0) && (file <= 8)) && ((rank > 0) && (rank <= 8))) {
+		this->str = this->_numberToLetter(file) + to_string(rank);
+		this->file = file;
+		this->rank = rank;
+
+		return;
+	}
+	throw exception("Invalid position!");
+}
+
+void Position::set(string posStr) {
+	if (posStr.length() == 2) {
+		int file = this->stringToFileAndRank(posStr).first;
+		int rank = this->stringToFileAndRank(posStr).second;
+
+		if (((file > 0) && (file <= 8)) && ((rank > 0) && (rank <= 8))) {
+			this->str = posStr;
+			this->file = file;
+			this->rank = rank;
+
+			return;
+		}
+	}
+	throw exception("Invalid position string!");
+}
+
+void Position::set(int file, int rank) {
+	if (((file > 0) && (file <= 8)) && ((rank > 0) && (rank <= 8))) {
+		this->str = this->_numberToLetter(file) + to_string(rank);
+		this->file = file;
+		this->rank = rank;
+
+		return;
+	}
+	throw exception("Invalid position!");
+}
+
+void Position::setFile(int file) {
+	if ((file > 0) && (file <= 8)) {
+		this->str = this->_numberToLetter(file) + this->rank;
+		this->file = file;
+		return;
+	}
+	throw exception("Invalid position!");
+}
+
+void Position::setFile(char file) {
+	int newFile = this->_letterToNumber(file);
+	if ((newFile > 0) && (newFile <= 8)) {
+		this->str = file + to_string(this->rank);
+		this->file = newFile;
+		return;
+	}
+	throw exception("Invalid position!");
+}
+
+void Position::setRank(int rank) {
+	if ((rank > 0) && (rank <= 8)) {
+		this->str = this->_numberToLetter(this->file) + to_string(rank);
+		this->rank = rank;
+
+		return;
+	}
+	throw exception("Invalid position!");
+}
---
+++ ChessCPP/Position/Position.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Position {
+private:
+	string str;
+	unsigned short file;
+	unsigned short rank;
+	static int _letterToNumber(char letter);
+	static char _numberToLetter(int number);
+public:
+	static pair<int, int> stringToFileAndRank(string notation);
+
+	string toString();
+
+	Position(string posStr);
+	Position(int file, int rank);
+
+	void set(string posStr);
+	void set(int file, int rank);
+
+	void setFile(int file);
+	void setFile(char file);
+
+	void setRank(int rank);
+};
+
