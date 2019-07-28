227d81fc7320273ad9decc3c69c1589df2f8c298
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2019-07-29 02:50:59 +0300

    feat(TimeMachine): add methods for adding new moves to the Time Machine

--- ChessCPP/TimeMachine/TimeMachine.cpp
+++ ChessCPP/TimeMachine/TimeMachine.cpp
@@ -1,1 +1,30 @@
 #include "TimeMachine.h"
+
+bool TimeMachine::resume() {
+	if (!this->isTimeTravelling && this->forward.empty()) {
+		return true;
+	}
+
+	if (this->forward.empty()) {
+		this->isTimeTravelling = false;
+		return true;
+	}
+
+	if (!this->forward.empty()) {
+		while (!this->forward.empty()) {
+			this->back.push(this->forward.top());
+			this->forward.pop();
+		}
+	}
+}
+
+void TimeMachine::addMove(Move* move) {
+	if (isTimeTravelling) {
+		// Since we are time travelling, adding a move will
+		// effectivelly start a new traversal tree, thus, resetting
+		// the previous "future" moves. That's why we have to clear
+		// the forwards stack.
+		this->forward = stack<Move*>();
+	}
+	this->back.push(move);
+}
--- ChessCPP/TimeMachine/TimeMachine.h
+++ ChessCPP/TimeMachine/TimeMachine.h
@@ -2,6 +2,15 @@
 #include <stack>
 #include "../Board/Board.h"
 class TimeMachine {
-	
+private:
+	stack<Move*> back;
+	stack<Move*> forward;
+	bool isTimeTravelling = false;
+
+public:
+	bool goBackwards();
+	bool goForwards();
+	bool resume();
+	void addMove(Move* move);
 };
 
