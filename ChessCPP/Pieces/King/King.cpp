9c5db8bd66425f43ec1499fafa15ec9e1da32a35
Konstantinos Kamaropoulos <kamaropoulos@outlook.com>
2019-07-31 17:40:14 +0300

    feat(King): add getAvailableMoves implementation

--- ChessCPP/Pieces/King/King.cpp
+++ ChessCPP/Pieces/King/King.cpp
@@ -15,6 +15,90 @@ King::King(Position* pos, Color color) : Piece(pos, color, 0) {
 }
 
 vector<Position*> King::getAvailableMoves(Board* board) {
-	cout << "getAvalableMoves for " << this->pieceName() << endl;
-	return vector<Position*>();
+	vector<Position*> availableMoves;
+
+	// Possible moves:
+	// up
+	// down
+	// left
+	// right
+	// up right
+	// up left
+	// down right
+	// down left
+
+	Position* origin = new Position(this->position);
+
+	Position* posToCheck = nullptr;
+
+	vector<Position*> movesToCheck;
+
+	// up
+	try {
+		posToCheck = new Position(origin->getFile(), origin->getRank() + 1);
+		movesToCheck.push_back(posToCheck);
+	}
+	catch (...) {}
+
+	// down
+	try {
+		posToCheck = new Position(origin->getFile(), origin->getRank() - 1);
+		movesToCheck.push_back(posToCheck);
+	}
+	catch (...) {}
+
+	// left
+	try {
+		posToCheck = new Position(origin->getFile() - 1, origin->getRank());
+		movesToCheck.push_back(posToCheck);
+	}
+	catch (...) {}
+
+	// right
+	try {
+		posToCheck = new Position(origin->getFile() + 1 , origin->getRank());
+		movesToCheck.push_back(posToCheck);
+	}
+	catch (...) {}
+
+	// up right
+	try {
+		posToCheck = new Position(origin->getFile() + 1, origin->getRank() + 1);
+		movesToCheck.push_back(posToCheck);
+	}
+	catch (...) {}
+
+	// up left
+	try {
+		posToCheck = new Position(origin->getFile() - 1, origin->getRank() + 1);
+		movesToCheck.push_back(posToCheck);
+	}
+	catch (...) {}
+
+	// down right
+	try {
+		posToCheck = new Position(origin->getFile() + 1, origin->getRank() - 1);
+		movesToCheck.push_back(posToCheck);
+	}
+	catch (...) {}
+
+	// down left
+	try {
+		posToCheck = new Position(origin->getFile() - 1, origin->getRank() - 1);
+		movesToCheck.push_back(posToCheck);
+	}
+	catch (...) {}
+
+	for (auto move : movesToCheck) {
+		MoveCheckResult res = this->_checkMove(move, board);
+		if (res != CANT_MOVE) {
+			availableMoves.push_back(move);
+		}
+	}
+
+	for (auto move : availableMoves) {
+		cout << move->toString() << endl;
+	}
+
+	return availableMoves;
 }
